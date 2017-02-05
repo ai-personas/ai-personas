a275ea991819386877672313b9f7ddc7ecb14c3b
Ramesh Ganesan <info2ram@gmail.com>
2017-02-05 16:47:38 -0500

    Physical - Theano changes

--- Physical/output.py
+++ Physical/output.py
@@ -5,35 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm 
 
-w_h1 = init_weights((40000, 400)) 
-w_h2 = init_weights((400, 200)) 
-w_h3 = init_weights((200, 40000)) 
-
-def model(X,w_h1,w_h2,w_h3,p_drop_input, p_drop_hidden):
-    X = dropout(X, p_drop_input) 
-    w_o_h1=rectify(T.dot(X, w_h1)) 
-    w_o_h1= dropout(w_o_h1, p_drop_input) 
-    w_o_h2=rectify(T.dot(w_o_h1, w_h2)) 
-    w_o_h2= dropout(w_o_h2, p_drop_input) 
-    y_x = softmax(T.dot(w_o_h2, w_h3)) 
-    return y_x 
-
-
-X = T.fmatrix() 
-Y = T.fmatrix() 
-
-y_x = model(X,w_h1,w_h2,w_h3)
-cost = T.mean(T.nnet.categorical_crossentropy(y_x, Y))
-params = [w_h1,w_h2,w_h3]
-updates = RMSprop(cost, params, lr=0.001) 
-train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True) 
-predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True) 
+srng = RandomStreams()
 
 def dropout(X, p=0.): 
     if p > 0: 
-    retain_prob = 1 - p 
-    X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX) 
-    X /= retain_prob 
+        retain_prob = 1 - p 
+        X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX) 
+        X /= retain_prob 
     return X 
 
 def rectify(X): 
@@ -61,3 +39,68 @@ def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
         updates.append((p, p - lr * g)) 
     return updates 
 
+w_h1 = init_weights((40000, 400)) 
+w_h2 = init_weights((400, 200)) 
+w_h3 = init_weights((200, 40000)) 
+
+def model(X,w_h1,w_h2,w_h3,p_drop_input=0.0, p_drop_hidden=0.0):
+    X = dropout(X, p_drop_input) 
+    w_o_h1=rectify(T.dot(X, w_h1)) 
+    w_o_h1= dropout(w_o_h1, p_drop_input) 
+    w_o_h2=rectify(T.dot(w_o_h1, w_h2)) 
+    w_o_h2= dropout(w_o_h2, p_drop_input) 
+    y_x = softmax(T.dot(w_o_h2, w_h3)) 
+    return y_x 
+
+
+X = T.fmatrix() 
+Y = T.fmatrix() 
+
+y_x = model(X,w_h1,w_h2,w_h3)
+cost = T.mean(T.nnet.categorical_crossentropy(y_x, Y))
+params = [w_h1,w_h2,w_h3]
+updates = RMSprop(cost, params, lr=0.001) 
+train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True) 
+predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True) 
+
+import PIL
+from PIL import Image
+import requests
+from io import BytesIO
+
+for i in range(10):
+    with open("../Environment/Informations/Content/Portraits/trainImages/images.txt") as f:
+        infoFileContents = f.readlines()
+        for l in range(len(infoFileContents)):
+            infoLine = infoFileContents[l].lstrip().rstrip()
+            response = requests.get(infoLine)
+
+            trainInpImg = Image.open(BytesIO(response.content))
+            trainInpGrayImg = trainInpImg.convert("L")
+            trainInpGrayImg = trainInpGrayImg.resize((200,200), PIL.Image.ANTIALIAS)
+            trainInpGreyImgArray = np.asarray(trainInpGrayImg, dtype=np.uint8).ravel()
+
+            trainOutImg = Image.open(BytesIO(response.content))
+            trainOutGrayImg = trainOutImg.convert("L")
+            trainOutGrayImg = trainOutGrayImg.resize((200,200), PIL.Image.ANTIALIAS)
+            trainOutGreyImgArray = np.asarray(trainOutGrayImg, dtype=np.uint8).ravel()
+
+            cost = train([trainInpGreyImgArray], [trainOutGreyImgArray])
+
+with open("../Environment/Informations/Content/Portraits//testImages/images.txt") as f:
+    infoFileContents = f.readlines()
+    for l in range(len(infoFileContents)):
+        infoLine = infoFileContents[l].lstrip().rstrip()
+        response = requests.get(infoLine)
+        
+        trainInpImg = Image.open(BytesIO(response.content))
+        trainInpGrayImg = trainInpImg.convert("L")
+        trainInpGrayImg = trainInpGrayImg.resize((200,200), PIL.Image.ANTIALIAS)
+        trainInpGreyImgArray = np.asarray(trainInpGrayImg, dtype=np.uint8).ravel()
+        
+        plt.imshow(trainInpGrayImg, cmap = cm.Greys_r)
+        plt.show()
+        
+        predImgArray = predict([trainInpGreyImgArray])
+        plt.imshow(predImgArray.reshape(200,200), cmap = cm.Greys_r)
+        plt.show()
