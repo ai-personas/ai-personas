5dbe44ab17eec9601f8764dd22ba3f6f6415181d
Ramesh Ganesan <info2ram@gmail.com>
2017-03-24 22:02:30 -0400

    keras physical using protobuf

--- Environment/Informations/Process/Extract/ImageURLExtractor.py
+++ Environment/Informations/Process/Extract/ImageURLExtractor.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 import requests
 from io import BytesIO
@@ -34,10 +34,12 @@ class ImageURLExtractor(object):
                 for parameter in inputTransform.transformParam:
                     if parameter.parameterName == 'color':
                         if parameter.parameterValue == 'grey':
+                            print ("transform grey")
                             img = img.convert("L")         
-                        if parameter.parameterName == 'process':
-                            if parameter.parameterValue == 'edge':
-                                img = img.filter(ImageFilter.FIND_EDGES)
+                    if parameter.parameterName == 'process':
+                        if parameter.parameterValue == 'edge':
+                            print ("transform edge")
+                            img = img.filter(ImageFilter.FIND_EDGES)
                             
                 img = np.asarray(img, dtype=np.float32)     
                 data[image_index, 0, :, :] = img
@@ -67,9 +69,9 @@ class ImageURLExtractor(object):
                     if parameter.parameterName == 'color':
                         if parameter.parameterValue == 'grey':
                             img = img.convert("L")         
-                        if parameter.parameterName == 'process':
-                            if parameter.parameterValue == 'edge':
-                                img = img.filter(ImageFilter.FIND_EDGES)
+                    if parameter.parameterName == 'process':
+                        if parameter.parameterValue == 'edge':
+                            img = img.filter(ImageFilter.FIND_EDGES)
                             
                 img = np.asarray(img, dtype=np.float32)     
                 data[image_index, 0, :, :] = img
@@ -79,4 +81,4 @@ class ImageURLExtractor(object):
         else:
             print ("Extractor in the information file and running extractor is not matching.")
         return data    
- 
+    
