09d20a0055785e1d2252ed4b084e783ffb7d632a
Ramesh Ganesan <info2ram@gmail.com>
2017-04-06 23:14:35 -0400

    extractor for image array

--- Environment/Informations/Process/Extract/extractImageFromImageArray.py
+++ Environment/Informations/Process/Extract/extractImageFromImageArray.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[24]:
+# In[14]:
 
 import sys
 import imp
@@ -10,31 +10,37 @@ import requests
 from io import BytesIO
 from PIL import Image, ImageFilter
 import numpy as np
+import logging
 
 INFORMATION_BLUEPRINT = "../../informationBlueprint"
 INFORMATION_BASE = "../../"
 PROTO_PYTHON_EXTENSION = "_pb2.py"
 PROTO_DEF_EXTENSION = ".bin"
 
+logger = logging.getLogger('Extractor')
+
 class Extractor(object):
         
-    def __init__(self, personaInformation):
-        self.personaInformation = personaInformation
+    def __init__(self, source):
+        self.source = source
         #load information
-        loadInformationDefinition(getInformationBlueprint())
+        self.informationDefinition = loadInformationDefinition(getInformationBlueprint())
         
-    def getInformationBlueprint(self):
+    def getInformationBlueprint(self, informationBluePrintPath):
         #information blueprint path
-        information_blueprint_path = os.path.abspath(os.path.join(PERSONA_BLUEPRINT + PROTO_PYTHON_EXTENSION))
+        information_blueprint_path = os.path.abspath(os.path.join(informationBluePrintPath))
+        logger.info("information blue print path: " + information_blueprint_path)
         #import information blueprint
         informationBlueprint = imp.load_source('Information', information_blueprint_path).Information()
         return informationBlueprint
         
-    def loadInformationDefinition(self, informationBluePrint):
+    def loadInformationDefinition(self, informationBluePrintPath):
         #load information
-        information_path = os.path.abspath(os.path.join(INFORMATION_BASE, self.personaInformation.informationSource + ".bin"))
+        information_path = os.path.abspath(os.path.join(self.source.sourceName + PROTO_DEF_EXTENSION))
+        logger.info("information path: " + information_path)
         f = open(information_path, "rb")
-        informationBluePrint.ParseFromString(f.read())
+        information = self.getInformationBlueprint()
+        information.ParseFromString(f.read())
         f.close()
         return informationBluePrint
 
@@ -43,99 +49,48 @@ class Extractor(object):
         
     def getExtractedData(self, inputTransform, images):
         return
-    
-    def getTrainingData(self, inputTransform):
-        for trainingData in self.information.trainingDataList:
-            response = requests.get(trainingData.URL)
-            img = Image.open(BytesIO(response.content))
 
-            #FIXME: move transformer to transform package
-            img_rows = inputTransform.transformSize[0].dimensionSize
-            img_cols = inputTransform.transformSize[1].dimensionSize
-            img = img.resize((img_rows,img_cols), Image.ANTIALIAS)
-
-            for parameter in inputTransform.transformParam:
-                if parameter.parameterName == 'color':
-                    if parameter.parameterValue == 'grey':
-                        print ("transform grey")
-                        img = img.convert("L")         
-                if parameter.parameterName == 'process':
-                    if parameter.parameterValue == 'edge':
-                        print ("transform edge")
-                        img = img.filter(ImageFilter.FIND_EDGES)
-
-            img = np.asarray(img, dtype=np.float32)     
-            data[image_index, 0, :, :] = img
-            print (img.shape)
-            print (trainingData.URL)
-                
-        return data
     
-    def getTestData(self, inputTransform):
-        no_of_images = len(self.information.testDataList)
-        data = np.random.random((no_of_images, self.dim, inputTransform.transformSize[0].dimensionSize, inputTransform.transformSize[1].dimensionSize))
-        image_index = 0
-        
-        if self.information.extractor == type(self).__name__:
-            for testData in self.information.testDataList:
-                response = requests.get(testData.URL)
-                img = Image.open(BytesIO(response.content))
-                
-                #FIXME: move transformer to transform package
-                img_rows = inputTransform.transformSize[0].dimensionSize
-                img_cols = inputTransform.transformSize[1].dimensionSize
-                img = img.resize((img_rows,img_cols), Image.ANTIALIAS)
-                
-                for parameter in inputTransform.transformParam:
-                    if parameter.parameterName == 'color':
-                        if parameter.parameterValue == 'grey':
-                            img = img.convert("L")         
-                    if parameter.parameterName == 'process':
-                        if parameter.parameterValue == 'edge':
-                            img = img.filter(ImageFilter.FIND_EDGES)
-                            
-                img = np.asarray(img, dtype=np.float32)     
-                data[image_index, 0, :, :] = img
-                print (img.shape)
-                print (testData.URL)
-                
-        else:
-            print ("Extractor in the information file and running extractor is not matching.")
-        return data   
-    
-    def getValidationData():
-        return
-    
-    def getActionData():
-        return
+# ext = Extractor()
+# ext.loadInformationDefinition(INFORMATION_BLUEPRINT)
 
 class test(object):
     
-    PERSONA_NAME_QUALIFIER = "PersonaDefinition"
-    TEST_PERSONA_BLUEPRINT = "../../../../Personas/personaBlueprint/version_1/personBlueprint" + PROTO_PYTHON_EXTENSION
-    TEST_PERSONA_NAME = "Khandhasamy" + PERSONA_NAME_QUALIFIER + PROTO_DEF_EXTENSION
-    TEST_PERSONA_DEF = "../../../Personas/Artist/Portraits/sketchToGreyImage/Khandhasamy/Evolution_1/age_1" + TEST_PERSONA_NAME
-
     def __init__(self):
         return 
         
-    def getPersonaBlueprint(self): 
+    def getPersonaBlueprint(self, personaBlueprintPath): 
         #persona blueprint path
-        persona_blueprint_path = os.path.abspath(os.path.join(self.TEST_PERSONA_BLUEPRINT))
+        persona_blueprint_path = os.path.abspath(os.path.join(personaBlueprintPath))
         print (persona_blueprint_path)
         #persona blueprint
         personaBlueprint = imp.load_source('Persona', persona_blueprint_path).Persona() 
         return personaBlueprint
     
-    def loadPersona(self):
-        persona = self.getPersonaBlueprint()
+    def loadPersona(self, personaBlueprintPath, personaDefPath):
+        # persona blueprint
+        persona = self.getPersonaBlueprint(personaBlueprintPath)
         #load persona
-        persona_path = os.path.abspath(os.path.join(self.TEST_PERSONA_DEF))
-        f = open("test.bin", "rb")
+        persona_path = os.path.abspath(os.path.join(personaDefPath))
+        print (persona_path)
+        f = open(personaDefPath, "rb")
         persona.ParseFromString(f.read())
         f.close()        
         return persona
     
+    def testExtractedData(self, personaBlueprintPath, personaDefPath):
+        persona = self.loadPersona(personaBlueprintPath, personaDefPath)
+        
+        return 
+    
+PERSONA_NAME_QUALIFIER = "PersonaDefinition"
+TEST_PERSONA_BLUEPRINT = "../../../../Personas/personaBlueprint/version_1/personBlueprint" + PROTO_PYTHON_EXTENSION
+TEST_PERSONA_NAME = "Khandhasamy" + PERSONA_NAME_QUALIFIER + PROTO_DEF_EXTENSION
+TEST_PERSONA_DEF = "../../../../Personas/Artist/Portraits/sketchToGreyImage/Khandhasamy/Evolution_1/age_1/" + TEST_PERSONA_NAME
+    
+tst = test()
+tst.loadPersona(TEST_PERSONA_BLUEPRINT, TEST_PERSONA_DEF)
+
 def testExtractor():
     #persona blueprint path
     information_blueprint_path = os.path.join("..", "..")
@@ -182,8 +137,64 @@ def testPopulatePersonaInformation():
 
 # testExtractor()
 
-testExtractor = test()
-testExtractor.loadPersona()
+def getTrainingData(self, inputTransform):
+    for trainingData in self.information.trainingDataList:
+        response = requests.get(trainingData.URL)
+        img = Image.open(BytesIO(response.content))
+
+        #FIXME: move transformer to transform package
+        img_rows = inputTransform.transformSize[0].dimensionSize
+        img_cols = inputTransform.transformSize[1].dimensionSize
+        img = img.resize((img_rows,img_cols), Image.ANTIALIAS)
+
+        for parameter in inputTransform.transformParam:
+            if parameter.parameterName == 'color':
+                if parameter.parameterValue == 'grey':
+                    print ("transform grey")
+                    img = img.convert("L")         
+            if parameter.parameterName == 'process':
+                if parameter.parameterValue == 'edge':
+                    print ("transform edge")
+                    img = img.filter(ImageFilter.FIND_EDGES)
+
+        img = np.asarray(img, dtype=np.float32)     
+        data[image_index, 0, :, :] = img
+        print (img.shape)
+        print (trainingData.URL)
+
+    return data
+
+def getTestData(self, inputTransform):
+    no_of_images = len(self.information.testDataList)
+    data = np.random.random((no_of_images, self.dim, inputTransform.transformSize[0].dimensionSize, inputTransform.transformSize[1].dimensionSize))
+    image_index = 0
+
+    if self.information.extractor == type(self).__name__:
+        for testData in self.information.testDataList:
+            response = requests.get(testData.URL)
+            img = Image.open(BytesIO(response.content))
+
+            #FIXME: move transformer to transform package
+            img_rows = inputTransform.transformSize[0].dimensionSize
+            img_cols = inputTransform.transformSize[1].dimensionSize
+            img = img.resize((img_rows,img_cols), Image.ANTIALIAS)
+
+            for parameter in inputTransform.transformParam:
+                if parameter.parameterName == 'color':
+                    if parameter.parameterValue == 'grey':
+                        img = img.convert("L")         
+                if parameter.parameterName == 'process':
+                    if parameter.parameterValue == 'edge':
+                        img = img.filter(ImageFilter.FIND_EDGES)
+
+            img = np.asarray(img, dtype=np.float32)     
+            data[image_index, 0, :, :] = img
+            print (img.shape)
+            print (testData.URL)
+
+    else:
+        print ("Extractor in the information file and running extractor is not matching.")
+    return data   
 
 
 # In[ ]:
---
+++ Personas/personaBlueprint/generatePersonaDefinition.py
@@ -0,0 +1,109 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import os, sys, inspect
+import copy
+import imp
+
+INSTALLATION_PATH = "C:\Users\rames\Documents\GitHub\ai-personas"
+
+PROTO_DEF_EXTENSION = ".bin"
+PROTO_PYTHON_EXTENSION = "_pb2.py"
+PERSONA_BLUEPRINT = "version_1/personBlueprint" + PROTO_PYTHON_EXTENSION
+PERSONA_NAME = "Khandhasamy"
+PERSONA_NAME_QUALIFIER = "PersonaDefinition"
+PERSONA_AGE = 1
+PERSONA_DEF_PATH="../Artist/Portraits/sketchToGreyImage/Khandhasamy/Evolution_1/age_" + str(PERSONA_AGE) + "/" + PERSONA_NAME + PERSONA_NAME_QUALIFIER + PROTO_DEF_EXTENSION
+DNA_BLUEPRINT = "../../DNA/dnaBlueprint/version_1/dnaBlueprint" +  PROTO_PYTHON_EXTENSION
+DNA_NAME_QUALIFIER = "DnaDefinition"
+DNA_NAME = "Khandhasamy" + DNA_NAME_QUALIFIER + PROTO_DEF_EXTENSION
+DNA_DEF_PATH = "../../DNA/dnaFamily/Khandhasamy/Evolution_1/" + DNA_NAME
+SOURCE_PATH = INSTALLATION_PATH + "\Environment\Informations\Category\Portraits\scientists.bin"
+
+class PersonaDefinitionGeneration(object):
+    
+    def getDnaBlueprint(self, dnaBlueprintPath):
+        #DNA blueprint path
+        dna_blueprint_path = os.path.abspath(os.path.join(dnaBlueprintPath))
+        print (dna_blueprint_path)
+        #dna blueprint
+        dnaBlueprint = imp.load_source('DNA', dna_blueprint_path).DNA() 
+        return dnaBlueprint
+    
+    def getDnaDefinition(self, dna, dnaDefPath):
+        # read dna definition
+        f = open(dnaDefPath, "rb")
+        dna.ParseFromString(f.read())
+        f.close()
+        return dna 
+
+    def getPersonaBlueprint(self, dnaBlueprintPath, dnaDefPath, personaBlueprintPath): 
+        #persona blueprint path
+        persona_blueprint_path = os.path.abspath(os.path.join(personaBlueprintPath))        
+        #persona blueprint
+        personaBlueprint = imp.load_source('Persona', persona_blueprint_path).Persona() 
+        return personaBlueprint
+
+    def savePersonaDefinition(self, personaDefPath, persona):
+        print (personaDefPath)
+        # write persona definition
+        f = open(personaDefPath, "wb")
+        f.write(persona.SerializeToString())
+        f.close()
+        
+    def generatePersonaDefinition(self, dnaBlueprintPath, dnaDefPath, personaBlueprintPath, personaDefPath):
+        persona = self.getPersonaBlueprint(dnaBlueprintPath, dnaDefPath, personaBlueprintPath)
+        dna = persona.DNAs.add()
+        # load dna definition
+        dna = self.getDnaDefinition(dna, dnaDefPath)
+        # generate physical 
+        self.generatePhysical(persona)
+        #generate age
+        self.generateAge(persona)
+        # save persona
+        self.savePersonaDefinition(personaDefPath, persona)
+        return
+    
+    def generatePhysical(self, persona):
+        persona.physical = "keras"
+        return persona
+    
+    def generateAge(self, persona):
+        persona.age.old = 1
+        persona.age.learningCycle = 50
+        persona.age.learningBatchSize = 3
+        self.generateEnvironment(persona)
+        return persona
+    
+    def generateEnvironment(self, persona):
+        environment = persona.age.environments.add()
+        environment.society = ""
+        self.generateLibrary(environment)
+        return environment
+    
+    def generateLibrary(self, environment):
+        source = environment.library.sources.add()
+        source.sourceName = SOURCE_PATH
+        self.generateSourceConnectionLayer(source)
+        return environment.library
+    
+    def generateSourceConnectionLayer(self, source):
+        sourceConnectionLayer = source.sourceConnectionLayers.add()
+        sourceConnectionLayer.connectedLayerName = "Layer1"
+        sourceConnectionLayer.imageSource.imageWidth = 50
+        sourceConnectionLayer.imageSource.imageHeight = 50
+        sourceConnectionLayer.imageSource.imageProcess.append("grey")
+        sourceConnectionLayer.imageSource.imageProcess.append("edge")
+        return sourceConnectionLayer
+        
+    
+personaDefinitionGeneration = PersonaDefinitionGeneration()
+personaDefinitionGeneration.generatePersonaDefinition(DNA_BLUEPRINT, DNA_DEF_PATH,  PERSONA_BLUEPRINT, PERSONA_DEF_PATH)
+
+
+# In[ ]:
+
+
+
